7881b42fd57e80cf76f3119a812c53231a7f4e4f
Alexander Amin <alex@alexamin.de>
2025-05-03 14:57:58 -0700

    Fixed llama schema

--- schemas/basketball_events_schema.py
+++ schemas/basketball_events_schema.py
@@ -11,8 +11,14 @@ BASKETBALL_EVENTS_SCHEMA = {
                     "items": {
                         "type": "object",
                         "properties": {
-                            "type": {"type": "string", "enum": [e.value for e in BasketballEvent]},
-                            "confidence": {"type": "number", "minimum": 0, "maximum": 1},
+                            "type": {
+                                "type": "string",
+                                "enum": [e.value for e in BasketballEvent]
+                            },
+                            "confidence": {
+                                "type": "number",
+                                "description": "0 no confidence. 1 maximum confidence"
+                            },
                             "timestamp": {
                                 "type": "number",
                                 "description": "The input phrase that made you decide that there was an event. Minimum 5 words."
