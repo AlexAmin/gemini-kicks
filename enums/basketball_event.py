12f2c060de221ab8710d04ad584e130db739f827
Olcay Buyan <olcay@television.ai>
2025-05-03 14:59:35 -0700

    refactored code

---
+++ enums/basketball_event.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class BasketballEvent(Enum):
+    FREE_THROW = "Free Throw"
+    FOUL = "Foul"
+    STEAL = "Steal"
+    TURNOVER = "Turnover"
+    TIMEOUT = "Timeout"
+    SUBSTITUTION = "Substitution"
